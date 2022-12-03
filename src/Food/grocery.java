d34263083d8acf2a67ce2447702680d40aa9ca40
RuchaChotaliaNEU <114604722+RuchaChotaliaNEU@users.noreply.github.com>
2022-12-02 22:23:32 -0500

    Add files via upload

---
+++ src/Food/grocery.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Food;
+
+/**
+ *
+ * @author diyabaldota
+ */
+public class grocery {
+    private String storeName ;
+    private String storeLocation;
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+    
+    
+    
+    
+}
---
+++ src/lifestyle/fashion.java
@@ -0,0 +1,39 @@
+
+package lifestyle;
+
+/**
+ *
+ * @author ruchachotalia
+ */
+public class fashion {
+    private String size;
+    private String gender;
+    private String type;
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    
+
+}
