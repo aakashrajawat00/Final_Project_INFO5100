a09da59f27a1644f474d111a78d2cf3d5aac1332
RuchaChotaliaNEU <114604722+RuchaChotaliaNEU@users.noreply.github.com>
2022-12-02 22:05:04 -0500

    electronics

---
+++ build/classes/lifestyle/fashion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lifestyle;
+
+/**
+ *
+ * @author ruchachotalia
+ */
+public class fashion {
+    private String type;
+    private String gender;
+    private String size;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
